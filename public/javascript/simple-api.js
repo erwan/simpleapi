e79bd238ff3bc051f13aaa0a605d330587bac212
Erwan Loisant <eloisant@gmail.com>
2015-11-27 11:35:30 +0100

    All docs of a type

--- public/javascript/simple-api.js
+++ public/javascript/simple-api.js
@@ -4,15 +4,14 @@ $(function() {
   $("#form-request").submit(function(event) {
     $("#result").html("");
     event.preventDefault();
-   // Get some values from elements on the page:
+    // Get some values from elements on the page:
     var $form = $( this ),
-      repository = $form.find( "input[name='repository']" ).val(),
-      docType = $form.find( "input[name='document-type']" ).val(),
-      uid = $form.find( "input[name='uid']" ).val(),
-      baseUrl = $form.attr( "action" );
+        docType = $form.find( "input[name='document-type']" ).val(),
+        uid = $form.find( "input[name='uid']" ).val(),
+        baseUrl = $form.attr( "action" );
 
 
-    var url = [baseUrl, repository, docType, uid].filter(
+    var url = [baseUrl, docType, uid].filter(
       function (val) {
         return val;
       }
@@ -30,4 +29,35 @@ $(function() {
       });
     });
   });
+
+  $("#form-request-2").submit(function(event) {
+    $("#result").html("");
+    event.preventDefault();
+   // Get some values from elements on the page:
+    var $form = $( this ),
+      docType = $form.find( "input[name='document-type']" ).val(),
+      baseUrl = $form.attr( "action" );
+
+    var url = [baseUrl, docType].filter(
+      function (val) {
+        return val;
+      }
+    ).join('/');
+    //api-call
+    var fullUrl = "http://" + window.location.host + url;
+    $("#api-call").html(fullUrl);
+
+    // Send the data using post
+    var getting = $.get( url, {} );
+    getting.done(function(data) {
+      $("#result").html(JSON.stringify(data, undefined, 4));
+      $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
+    });
+  });
+
+
+
+
 });
